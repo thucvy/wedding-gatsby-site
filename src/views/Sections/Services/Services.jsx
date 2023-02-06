a2c5857006962556becbfdb5937f71a24c755028
thuccvy <thuccvy@gmail.com>
2023-02-06 07:00:21 +0700

    fix content

--- src/views/Sections/Services/Services.jsx
+++ src/views/Sections/Services/Services.jsx
@@ -21,17 +21,10 @@ const Services = ({ className, frontmatter }) => {
       <Row className="text-center">
         <div>
           <p>
-            2020 has been a challenging year for everyone, but one good thing that has come out of
-            it for us has been getting engaged. With so much unknown about the future, we
-            didn&apos;t want to wait to get married and have decided to have a very small ceremony
-            with immediate family this year. We wish we could have invited you to celebrate with us,
-            but want to prioritize the health of our loved ones above anything else. With that in
-            mind, we have decided to live stream the ceremony so that anyone interested can join us
-            virtually.
+            Write something here
           </p>
           <p>
-            The ceremony and live stream will be taking place on December 13 at 2pm EST, and the
-            video will also be viewable afterwards.
+            The ceremony will be taking place on March 04 at 18:00
           </p>
           <p>
             We hope you can join us, whether live for the virtual event or down the road when we
@@ -40,7 +33,7 @@ const Services = ({ className, frontmatter }) => {
           {/* <br /> */}
           <p>Sending our love,</p>
           {/* <br /> */}
-          <p>Caitlin & Justin</p>
+          <p>Vy & Huân</p>
         </div>
       </Row>
     </PageSection>
